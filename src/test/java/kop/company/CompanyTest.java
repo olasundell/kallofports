14eee045a1cce196c4c72642abeed7a6631294eb
olasundell <ola.sundell@gmail.com>
2011-04-14 08:31:41 +0200

    added even more tests

---
+++ src/test/java/kop/company/CompanyTest.java
@@ -0,0 +1,27 @@
+package kop.company;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ola
+ * Date: 4/13/11
+ * Time: 9:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CompanyTest {
+	@Test
+	public void testMoveShips() throws Exception {
+
+	}
+
+	@Test
+	public void testDoDailyCosts() throws Exception {
+
+	}
+
+	@Test
+	public void testDoMonthlyCosts() throws Exception {
+
+	}
+}
